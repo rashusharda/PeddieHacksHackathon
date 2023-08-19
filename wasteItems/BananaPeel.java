d00e99771e645dd75a6db4cc50e4d2e73022e3af
Rashu <68036518+rashusharda@users.noreply.github.com>
2023-08-19 02:51:57 -0400

    Update BananaPeel.java
    Added comments.

--- wasteItems/BananaPeel.java
+++ wasteItems/BananaPeel.java
@@ -1,21 +1,29 @@
 package model.wasteItems;
 
+// This class represents a Banana Peel waste item
 public class BananaPeel extends WasteItem {
-  public static final String belongsTo = "Food Scrap Bin";
-  
-  private static final int code = 2;
-  
-  private static final String IMAGE_PATH = "src/data/images/waste_items/banana_peel.png";
-  
-  public BananaPeel(int x) {
-    super(x);
-    this.belongedBin = "Food Scrap Bin";
-    this.codeNum = 2;
-    this.name = "Banana peel";
-    this.imagePath = "src/data/images/waste_items/banana_peel.png";
-  }
-  
-  protected String getPath() {
-    return "src/data/images/waste_items/banana_peel.png";
-  }
+
+    // String indicating the bin that the Banana Peel belongs to
+    public static final String belongsTo = "Food Scrap Bin";
+
+    // Unique code for the Banana Peel waste item
+    private static final int code = 2;
+
+    // Path to the image file of the Banana Peel
+    private static final String IMAGE_PATH = "src/data/images/waste_items/banana_peel.png";
+
+    // Constructor to create a Banana Peel waste item
+    public BananaPeel(int x) {
+        super(x); // Initialize the waste item with the x-coordinate
+        this.belongedBin = "Food Scrap Bin"; // Set the bin that the waste item belongs to
+        this.codeNum = 2; // Set the code number
+        this.name = "Banana peel"; // Set the name of the waste item
+        this.imagePath = "src/data/images/waste_items/banana_peel.png"; // Set the image path
+    }
+
+    // Get the path to the waste item image
+    @Override
+    protected String getPath() {
+        return "src/data/images/waste_items/banana_peel.png";
+    }
 }
