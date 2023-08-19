57b7c21885fbe812fde4d45d667243b5ce17a80a
Rashu <68036518+rashusharda@users.noreply.github.com>
2023-08-19 02:56:28 -0400

    Update PlasticBag.java
    Added comments.

--- wasteItems/PlasticBag.java
+++ wasteItems/PlasticBag.java
@@ -5,33 +5,42 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
+// This class represents a Plastic Bag waste item
 public class PlasticBag extends WasteItem {
+
+    // Unique code for the Plastic Bag waste item
     private static final int code = 4;
+    
+    // String indicating the bin that the Plastic Bag belongs to
     public static final String belongsTo = "Garbage Bin";
 
-    //Icons made by <a href="https://www.flaticon.com/authors/good-ware"
-    // title="Good Ware">Good Ware</a> from
-    // <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    // Path to the image file of the Plastic Bag
+    // Icons made by Good Ware from www.flaticon.com
     private static final String IMAGE_PATH = "src/data/images/waste_items/plastic_bag.png";
 
+    // Constructor to create a Plastic Bag waste item
     public PlasticBag(int x) {
-        super(x);
-        belongedBin = belongsTo;
-        codeNum = code;
-        this.name = "Plastic Bag";
-        imagePath = IMAGE_PATH;
+        super(x); // Initialize the waste item with the x-coordinate
+        belongedBin = belongsTo; // Set the bin that the waste item belongs to
+        codeNum = code; // Set the code number
+        this.name = "Plastic Bag"; // Set the name of the waste item
+        imagePath = IMAGE_PATH; // Set the image path
+        
         try {
-            i = ImageIO.read(new File(imagePath)).getScaledInstance(SIZE_X,SIZE_Y, Image.SCALE_DEFAULT);
+            // Load the waste item image
+            i = ImageIO.read(new File(imagePath)).getScaledInstance(SIZE_X, SIZE_Y, Image.SCALE_DEFAULT);
         } catch (IOException e) {
             System.out.println("Fail to read image of items");
             e.printStackTrace();
         }
-
     }
 
+    // Get the path to the waste item image
     @Override
     protected String getPath() {
         return IMAGE_PATH;
     }
 
+    // The render method is inherited from the WasteItem class and does not need to be overridden
+    // It will render the waste item image along with its name
 }
