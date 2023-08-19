a634d8f229605d99a333379b69b05b0056dad88c
Rashu <68036518+rashusharda@users.noreply.github.com>
2023-08-19 02:38:48 -0400

    Update GamePanel.java
    Added comments.

--- GamePanel.java
+++ GamePanel.java
@@ -8,26 +8,24 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-// The game is rendered here.
-
+// This class represents the panel where the game is rendered.
 public class GamePanel extends JPanel {
+    // Constants for game over messages
     private static final String OVER = "Game Over!";
     private static final String REPLAY = "R to replay";
 
-    //<a href="https://www.freepik.com/free-photos-vectors/tree">
-    // Tree vector created by upklyak - www.freepik.com</a>
+    // Image source for the background
     private static final String imagePath = "src/data/images/background.jpg";
-    private WSGame game;
-    private Image bgImg;
 
-    // Constructs a game panel
-    // effects:  sets size and background colour of panel,
-    //           updates this with the game to be displayed
+    private WSGame game; // The game logic
+    private Image bgImg; // Background image
+
+    // Constructor
     public GamePanel(WSGame g) {
-        setPreferredSize(new Dimension(WSGame.WIDTH, WSGame.HEIGHT));
-        this.game = g;
+        setPreferredSize(new Dimension(WSGame.WIDTH, WSGame.HEIGHT)); // Set the panel size
+        this.game = g; // Set the game logic
         try {
-            bgImg = ImageIO.read(new File(imagePath)).getScaledInstance(WSGame.WIDTH,WSGame.HEIGHT,Image.SCALE_DEFAULT);
+            bgImg = ImageIO.read(new File(imagePath)).getScaledInstance(WSGame.WIDTH, WSGame.HEIGHT, Image.SCALE_DEFAULT);
         } catch (IOException e) {
             System.out.print("fail to load background image");
             e.printStackTrace();
@@ -38,39 +36,33 @@ public class GamePanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawImage(bgImg,0,0,null);
-        renderGame(g);
+        g.drawImage(bgImg, 0, 0, null); // Draw the background image
+        renderGame(g); // Draw the game elements
 
-        if (game.isOver()){
-            gameOver(g);
+        if (game.isOver()) {
+            gameOver(g); // Display game over message
         }
     }
 
-    // Draws the "game over" message and replay instructions
-    // modifies: g
-    // effects:  draws "game over" and replay instructions onto g
+    // Draw the "game over" message and replay instructions
     private void gameOver(Graphics g) {
-        Color saved = g.getColor();
-        g.setColor(new Color( 0, 0, 0));
-        g.setFont(new Font("Arial", 20, 20));
-        FontMetrics fm = g.getFontMetrics();
-        centreString(OVER, g, fm, WSGame.HEIGHT / 2);
-        centreString(REPLAY, g, fm, WSGame.HEIGHT / 2 + 40);
-        g.setColor(saved);
+        Color saved = g.getColor(); // Save the current color
+        g.setColor(new Color(0, 0, 0)); // Set color for text
+        g.setFont(new Font("Arial", Font.BOLD, 20)); // Set font for text
+        FontMetrics fm = g.getFontMetrics(); // Get font metrics for text positioning
+        centreString(OVER, g, fm, WSGame.HEIGHT / 2); // Display "Game Over!" centered
+        centreString(REPLAY, g, fm, WSGame.HEIGHT / 2 + 40); // Display "R to replay" centered below
+        g.setColor(saved); // Restore the original color
     }
 
-    // Draws the game
-    // modifies: g
-    // effects:  the game is drawn onto the Graphics object g
+    // Draw the game elements
     private void renderGame(Graphics g) {
-        game.render(g);
+        game.render(g); // Call the game's render method to draw game elements
     }
 
-    // Centres a string on the screen
-    // modifies: g
-    // effects:  centres the string str horizontally onto g at vertical position yPos
+    // Center a string on the screen
     private void centreString(String str, Graphics g, FontMetrics fm, int yPos) {
-        int width = fm.stringWidth(str);
-        g.drawString(str, (WSGame.WIDTH - width) / 2, yPos);
+        int width = fm.stringWidth(str); // Get the width of the string
+        g.drawString(str, (WSGame.WIDTH - width) / 2, yPos); // Draw the string centered horizontally
     }
 }
