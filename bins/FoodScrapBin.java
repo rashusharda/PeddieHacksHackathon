180c6072f063554a9b983adbe9757315efbf03e2
Rashu <68036518+rashusharda@users.noreply.github.com>
2023-08-19 02:44:42 -0400

    Update FoodScrapBin.java
    Added comments.

--- bins/FoodScrapBin.java
+++ bins/FoodScrapBin.java
@@ -1,36 +1,28 @@
 package model.bins;
 
-import java.awt.*;
+import java.awt.Color;
 
+// This class represents the Food Scrap Bin
 public class FoodScrapBin extends Bin {
 
     private static final Color COLOR = new Color(16, 188, 83);
     private static final String IMAGE_PATH = "src/data/images/bins/food_scrap_bin_green.png";
+
+    // Constructor
     public FoodScrapBin() {
-        super("Food Scrap \nBin",100);
-        color = COLOR;
-//        imagePath = IMAGE_PATH;
-//        {
-//            try {
-//                i = ImageIO.read(new File(imagePath)).getScaledInstance(SIZE_X,SIZE_Y,Image.SCALE_DEFAULT);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                System.out.print("fail to read image of bins");
-//            }
-//        }
+        super("Food Scrap \nBin", 100); // Initialize the bin with the name and x-coordinate
+        color = COLOR; // Set the color of the bin
+
+        // Load the bin image
+        imagePath = IMAGE_PATH;
     }
 
+    // Get the path to the bin image
     @Override
     protected String getPath() {
         return IMAGE_PATH;
     }
 
-
-//    @Override
-//    public void render(Graphics g) {
-//        Color savedCol = g.getColor();
-//        g.setColor(COLOR);
-//        g.fillRect(getX() - SIZE_X / 2, getY() - SIZE_Y / 2, SIZE_X, SIZE_Y);
-//        g.setColor(savedCol);
-//    }
+    // The render method is inherited from the Bin class and does not need to be overridden
+    // It will render the bin image along with its name
 }
