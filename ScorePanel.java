2aa9e36329d998dea4326510d2dc28210d08e048
Rashu <68036518+rashusharda@users.noreply.github.com>
2023-08-19 02:40:52 -0400

    Update ScorePanel.java
    Added comments.

--- ScorePanel.java
+++ ScorePanel.java
@@ -4,47 +4,94 @@ import model.WSGame;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.ArrayList;
+import java.util.Collection;
 
-public class ScorePanel extends JPanel implements Observer {
-    private static final String CORRECT_TXT = "Waste correctly sorted: ";
-    private static final String INCORRECT_TXT = "Waste misplaced: ";
-    private static final int LBL_WIDTH = 200;
-    private static final int LBL_HEIGHT = 30;
+// This class represents the instruction panel where players can choose difficulty levels.
+public class InstructionFrame extends JFrame {
 
-    private JLabel correctLbl;
-    private JLabel misplacedLbl;
+    private JButton startButton; // Button to start the game
+    Collection<JButton> buttons; // Collection to hold difficulty level buttons
 
-    // to construct a score panel
-    // effects: set the background color and renders the initial labels;
-    //          updates this with the scores
-    public ScorePanel(WSGame game) {
-        setBackground(new Color(255, 255, 255));
-        correctLbl = new JLabel(CORRECT_TXT + 0);
-        correctLbl.setPreferredSize(new Dimension(LBL_WIDTH,LBL_HEIGHT));
-        misplacedLbl = new JLabel(INCORRECT_TXT + 0);
-        misplacedLbl.setPreferredSize(new Dimension(LBL_WIDTH,LBL_HEIGHT));
-        add(correctLbl);
-        add(Box.createVerticalStrut(10));
-        add(misplacedLbl);
+    public static final String gameStarts = "Start the Game!"; // Button text for starting the game
+    private JPanel instPanel; // Panel for the instruction content
+    private Dimension dm = new Dimension(300, 240); // Dimension for panel size
+
+    // Constructor
+    public InstructionFrame() {
+        super();
+        setBackground(new Color(184, 184, 184)); // Set background color
+        setSize(dm); // Set the size of the frame
+        centreOnScreen(); // Center the frame on the screen
+        setVisible(true); // Make the frame visible
+
+        startButton = new JButton(gameStarts); // Create the start button
+        instPanel = new JPanel(); // Panel to hold the instruction content
+        instPanel.setLayout(new BoxLayout(instPanel, BoxLayout.Y_AXIS)); // Set panel layout
+
+        startButton.setActionCommand(gameStarts); // Set action command for the start button
+        startButton.setAlignmentX(Component.CENTER_ALIGNMENT); // Center align the start button
+        startButton.addActionListener(e -> {
+            dispose(); // Close the instruction frame
+            new WasteSortingGame(); // Start the game by opening the main game window
+        });
+
+        String instructions = "Please choose from a difficulty level: ";
+        String welcome = "Welcome to the Waste Sorting Game!";
+        String inst1 = "Use <- or -> key to control the position";
+        String inst2 = "Use down arrow or space key to speed up waste falling";
+
+        // Add instruction labels and content to the instruction panel
+        addALabel(welcome, instPanel);
+        addALabel(inst1, instPanel);
+        addALabel(inst2, instPanel);
+        addALabel(instructions, instPanel);
+        createButtons(); // Create difficulty level buttons
+        instPanel.add(startButton); // Add the start button to the panel
+        add(instPanel, BorderLayout.CENTER); // Add the instruction panel to the frame
+        pack(); // Pack the components
     }
 
-    // Updates the score panel
-    // modifies: this
-    // effects:  updates number of items correctly sorted and number of waste items misplaced
-    @Override
-    public void update(Observable o, Object arg) {
-        if (WSGame.CORRECTLY_SORTED.equals(arg) || WSGame.MISPLACED.equals(arg)) {
-            WSGame game = (WSGame) o;
-            correctLbl.setText(CORRECT_TXT + game.getCorrectItems());
-            misplacedLbl.setText(INCORRECT_TXT + game.getIncorrectItems());
-            repaint();
-        } else if (WSGame.GameStarts.equals(arg)) {
-            WSGame game = (WSGame) o;
-            correctLbl.setText(CORRECT_TXT + 0);
-            misplacedLbl.setText(INCORRECT_TXT + 0);
-            repaint();
+    // Create difficulty level buttons
+    private void createButtons() {
+        String[] levels = {"Easy", "Medium", "Difficult"};
+        buttons = new ArrayList<>();
+
+        // Create buttons for each difficulty level
+        for (String str : levels) {
+            JButton btn = new JButton(str);
+            buttons.add(btn);
+            btn.setActionCommand(str);
+            btn.setAlignmentX(Component.CENTER_ALIGNMENT); // Center align the buttons
+            btn.addActionListener(e -> {
+                btn.setSelected(true);
+                unselectRest(btn); // Deselect other buttons
+                WSGame.setDifficultyLevel(e); // Set the game difficulty level
+            });
+
+            instPanel.add(btn); // Add the button to the instruction panel
+        }
+    }
+
+    // Add a label to a container with centered alignment
+    private void addALabel(String text, Container container) {
+        JLabel label = new JLabel(text);
+        label.setAlignmentX(Component.CENTER_ALIGNMENT); // Center align the label
+        container.add(label); // Add the label to the container
+    }
+
+    // Center the frame on the screen
+    private void centreOnScreen() {
+        Dimension scrn = Toolkit.getDefaultToolkit().getScreenSize(); // Get screen dimensions
+        setLocation((scrn.width - getWidth()) / 2, (scrn.height - getHeight()) / 2); // Calculate center position
+    }
+
+    // Deselect other buttons when a button is selected
+    private void unselectRest(JButton btn) {
+        for (JButton b : buttons) {
+            if (!b.equals(btn)) {
+                b.setSelected(false);
+            }
         }
     }
 }
