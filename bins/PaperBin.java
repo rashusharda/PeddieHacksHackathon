89d49a2a635ff7f3f5fdc4717d78232859c0cb8e
Rashu <68036518+rashusharda@users.noreply.github.com>
2023-08-19 02:48:58 -0400

    Update PaperBin.java
    Added comments.

--- bins/PaperBin.java
+++ bins/PaperBin.java
@@ -1,27 +1,31 @@
 package model.bins;
 
-import java.awt.*;
+import java.awt.Color;
 
+// This class represents the Paper Bin
 public class PaperBin extends Bin {
 
+    // Color of the Paper Bin
     private static final Color COLOR = new Color(46, 81, 175);
+
+    // Path to the image file of the Paper Bin
     private static final String IMAGE_PATH = "src/data/images/bins/paper_bin_blue.png";
+
+    // Constructor to create a Paper Bin
     public PaperBin() {
-        super("Paper Bin",500);
-        this.color = COLOR;
+        super("Paper Bin", 500); // Initialize the bin with the name and x-coordinate
+        this.color = COLOR; // Set the color of the bin
+
+        // Load the bin image
         imagePath = IMAGE_PATH;
-//        {
-//            try {
-//                i = ImageIO.read(new File(imagePath)).getScaledInstance(SIZE_X,SIZE_Y,Image.SCALE_DEFAULT);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                System.out.print("fail to read image of bins");
-//            }
-//        }
     }
 
+    // Get the path to the bin image
     @Override
     protected String getPath() {
         return IMAGE_PATH;
     }
+
+    // The render method is inherited from the Bin class and does not need to be overridden
+    // It will render the bin image along with its name
 }
