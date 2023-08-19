73572ac9f25a2446ebbaa9c2e7e215cad6051f14
Rashu <68036518+rashusharda@users.noreply.github.com>
2023-08-19 02:47:08 -0400

    Update GarbageBin.java
    Added comments.

--- bins/GarbageBin.java
+++ bins/GarbageBin.java
@@ -1,28 +1,28 @@
 package model.bins;
 
-import java.awt.*;
+import java.awt.Color;
 
+// This class represents the Garbage Bin
 public class GarbageBin extends Bin {
 
     private static final Color COLOR = new Color(23, 23, 23);
     private static final String IMAGE_PATH = "src/data/images/bins/garbage_bin.png";
 
+    // Constructor
     public GarbageBin() {
-        super("Garbage Bin",700);
-        this.color = COLOR;
+        super("Garbage Bin", 700); // Initialize the bin with the name and x-coordinate
+        color = COLOR; // Set the color of the bin
+
+        // Load the bin image
         imagePath = IMAGE_PATH;
-//        {
-//            try {
-//                i = ImageIO.read(new File(imagePath)).getScaledInstance(SIZE_X,SIZE_Y,Image.SCALE_DEFAULT);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                System.out.print("fail to read image of bins");
-//            }
-//        }
     }
 
+    // Get the path to the bin image
     @Override
     protected String getPath() {
         return IMAGE_PATH;
     }
+
+    // The render method is inherited from the Bin class and does not need to be overridden
+    // It will render the bin image along with its name
 }
